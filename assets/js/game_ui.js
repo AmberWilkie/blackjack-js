d2fcffd628038f2be3b7c1d28f7b95eaffd5291f
Amber <amber@amberwilkie.com>
2016-08-21 18:27:44 +0200

    Fix dealer > 22 as a win -bug

--- assets/js/game_ui.js
+++ assets/js/game_ui.js
@@ -53,16 +53,18 @@ function hitMeClicked() {
 
 function holdClicked() {
   $('#hold').click(function () {
-    // $('#hit_me').click(preventDefault());
+    $("#hit_me").click(function(event){
+      event.preventDefault();
+    });
     while (dealer.hand_value <= 17) {
         addDealerCard();
       }
-    if (dealer.hand_value > player_one.hand_value) {
+    if ((dealer.hand_value > player_one.hand_value) && (dealer.hand_value < 22)) {
       $('#final_message').text('Dealer wins!');
-    } else if (dealer.hand_value < player_one.hand_value) {
-      $('#final_message').text('Player wins!');
-    } else {
+    } else if (dealer.hand_value == player_one.hand_value) {
       $('#final_message').text('Push!');
+    } else {
+      $('#final_message').text('Player wins!');
     }
     });
   // });
@@ -91,7 +93,10 @@ function changeMessage() {
   if (player_one.hand_value < 22) {
   game_message = ("Here's the player's total: " + player_one.message + " and here's the dealer's total: " + dealer.message);
   } else {
-  game_message = "Bust!!!!";
+    game_message = "Bust!!!!";
+    $("#hit_me").click(function(event){
+      event.preventDefault();
+    });
   }
   $('#display_message').text(game_message);
 }
