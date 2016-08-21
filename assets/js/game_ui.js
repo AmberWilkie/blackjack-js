8a349b6b48b0af55d5d90a3ca29e65c6db996e2c
Amber <amber@amberwilkie.com>
2016-08-21 18:37:09 +0200

    Conceal dealer's first card

--- assets/js/game_ui.js
+++ assets/js/game_ui.js
@@ -21,7 +21,7 @@ function dealClicked() {
     $("#deal").click(function () {
       $('#player_card_one').text(player_one.player_hand[0]);
       $('#player_card_two').text(player_one.player_hand[1]);
-      $('#dealer_card_one').text(dealer.player_hand[0]);
+      $('#dealer_card_one').text('Concealed');
       $('#dealer_card_two').text(dealer.player_hand[1]);
       player_one.calculateHandTotal();
       dealer.calculateHandTotal();
@@ -53,10 +53,8 @@ function hitMeClicked() {
 
 function holdClicked() {
   $('#hold').click(function () {
-    $("#hit_me").click(function(event){
-      event.preventDefault();
-    });
-    while (dealer.hand_value <= 17) {
+    $('#dealer_card_one').text(dealer.player_hand[0]);
+    while (dealer.hand_value < 17) {
         addDealerCard();
       }
     if ((dealer.hand_value > player_one.hand_value) && (dealer.hand_value < 22)) {
@@ -66,8 +64,7 @@ function holdClicked() {
     } else {
       $('#final_message').text('Player wins!');
     }
-    });
-  // });
+  });
 }
 
 function addDealerCard() {
@@ -94,9 +91,8 @@ function changeMessage() {
   game_message = ("Here's the player's total: " + player_one.message + " and here's the dealer's total: " + dealer.message);
   } else {
     game_message = "Bust!!!!";
-    $("#hit_me").click(function(event){
-      event.preventDefault();
-    });
+    $('#hit_me').hide();
+    $('#hold').hide();
   }
   $('#display_message').text(game_message);
 }
